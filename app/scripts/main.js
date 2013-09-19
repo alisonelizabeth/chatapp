b3d10d0caec4a25bf37a53a8908f011c95884907
alisonelizabeth <alisonmllr20@gmail.com>
2013-09-18 21:21:39 -0400

    Created MessageClass and MessageClss Collection; created save function

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,37 @@
 Parse.initialize("6Uj7OjjpZsh1pwxjc6CJq8h2VC5Mzx0p7LlUNl6C", "B2aoeUOMbKXD9Nc6aCpveM4CWHgXrduugkuYcBuh");
 
-// var TestObject = Parse.Object.extend("TestObject");
-// var testObject = new TestObject();
-// testObject.save({foo: "bar"}, {
-//   success: function(object) {
-//     alert("yay! it worked");
-//   }
-// });
+var MessageClass = Parse.Object.extend('MessageClass');
+var MessageClassCollection = Parse.Collection.extend({
+	model: MessageClass
+});
+
+var messages = new MessageClassCollection();
+
+$('document').ready(function() {
+	$('.submit').click(function(event){
+		event.preventDefault();
+		var message = new MessageClass();
+		var messageVal = $('.message-input').val();
+		message.set('message', messageVal);
+
+		message.save(null, {
+			success: function(results) {
+				console.log(results)
+			},
+			error: function(results, error){
+				console.log(error.description)
+			}
+		});
+	});
+
+
+}); // end of document ready 
+
+
+
+
+
+
+
+
+
